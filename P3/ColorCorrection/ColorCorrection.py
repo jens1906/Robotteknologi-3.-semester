dd2563c985990f7c9a6d4f4478a512d2ad216a66
Andr465m <Andre_vester@yahoo.com>
2024-10-29 10:07:48 +0100

    CC on the image

--- P3/ColorCorrection/ColorCorrection.py
+++ P3/ColorCorrection/ColorCorrection.py
@@ -3,12 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 global CCtesting
 CCtesting = True
+import os
+os.system('cls')
 
 print("Color Correction Testing:", CCtesting)
+print("-----------------------------")
 
+normboard = cv.imread('P3\ColorCorrection\Color-Checker.jpg')
+#fuckboard = 
 if CCtesting == True:
-    normboard = cv.imread('P3\ColorCorrection\Color-Checker.jpg')
-    #fuckboard = 
+    
     plt.imshow(normboard) 
-    plt.show()
+    #plt.show()
 
+#find rgb value for each middle in each tile
+rows = 4
+cols = 6
+pixwidth, pixheight, dim = normboard.shape
+print(pixwidth, pixheight)
+tile_width = pixwidth // cols
+tile_height = pixheight // rows
+
+for c in range(cols):
+    for r in range(rows):
+        mid_x = c * tile_width + tile_width // 2
+        mid_y = r * tile_height + tile_height // 2
+
+        print(mid_x, mid_y)
+        rgb_values = normboard[mid_y, mid_x]
+        print(f"Tile ({r+1}, {c+1}) - RGB: {rgb_values}")
+        print("---------")
+
+ 
+
+ #plt.show()
