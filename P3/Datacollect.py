17cfa4485c09b883da4781506cd2fd5ca44cc6b3
DjangoLan <thor2617@gmail.com>
2024-11-16 10:55:10 +0100

    ForDataCollection

--- P3/Datacollect.py
+++ P3/Datacollect.py
@@ -1,16 +1,29 @@
 from Input import input as im
-
 import os
 import cv2 as cv
-import vmbpy as vmb
 from datetime import datetime
 
-#image = im.get_image()
-image = cv.imread('P3\Results\OrgImages\image_20241311_144042.png')
-#make folder in P3/Results/Data
-test_folder = 'maelk'
-test_name = 'forfra'
+image = im.get_image()
+#Remember.. exposure = 20-30k, gain = 0 i API
+#image = cv.imread('P3\Results\OrgImages\image_20241311_144042.png')
 
+
+
+#Light positions: 
+# Pink = Behind_Camera 
+# Dark blue = Beside_Camera
+# Orange = Right_Side
+# Light Green = Left_Side
+# Purple = Top_Down
+# Light Blue = InFront_Camera
+
+#Water
+#1/2 Milk
+#Milk
+
+#make folder in P3/Results/Data
+test_folder = 'Water'
+test_name = 'Beside_Camera'
 
 #make folder in P3/Results/Data
 try:
