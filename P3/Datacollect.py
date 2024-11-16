7196423f84caeeb4975af837ef9e37c51b94834f
DjangoLan <thor2617@gmail.com>
2024-11-16 12:19:24 +0100

    Data!

--- P3/Datacollect.py
+++ P3/Datacollect.py
@@ -8,9 +8,12 @@ image = im.get_image()
 #image = cv.imread('P3\Results\OrgImages\image_20241311_144042.png')
 
 
+#   Setup settings
+#Exposure 25k, Gain 0
+#Light = MAX
 
 #Light positions: 
-# Pink = Behind_Camera 
+# Pink = Behind_Camera
 # Dark blue = Beside_Camera
 # Orange = Right_Side
 # Light Green = Left_Side
@@ -18,12 +21,16 @@ image = im.get_image()
 # Light Blue = InFront_Camera
 
 #Water
-#1/2 Milk
-#Milk
+#1/32 Milk, mixed
+#1/16 Milk, mixed
+#1/8 Milk, mixed
+#1/4 Milk, mixed
+#1/2 Milk, mixed
+#Full Milk, mixed
 
 #make folder in P3/Results/Data
-test_folder = 'Water'
-test_name = 'Beside_Camera'
+test_folder = '32th_Milk'
+test_name = 'Behind_Camera'
 
 #make folder in P3/Results/Data
 try:
@@ -32,4 +39,4 @@ except FileExistsError:
     pass
 
 timestamp = datetime.now().strftime("%Y%d%m_%H%M%S")
-cv.imwrite(f'P3/Results/Data/{test_folder}/{test_name}_{timestamp}.png', image)
+cv.imwrite(f'P3/Results/Data/{test_folder}/{test_name}_{timestamp}.png', cv.cvtColor(image, cv.COLOR_BGR2RGB))
