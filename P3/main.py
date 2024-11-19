62dbe9d76bf96ff9767049de94aeda0b1a5f8399
André Vester Magnusson <77392011+Andr465m@users.noreply.github.com>
2024-11-19 09:29:57 +0100

    remade picture

--- P3/main.py
+++ P3/main.py
@@ -169,15 +169,7 @@ if __name__ == '__main__':
 
 
     if test_method == 'single':
-<<<<<<<<< Temporary merge branch 1
-<<<<<<< Updated upstream
-        image_path = 'P3/Results/Data/16th_Milk/Right_Side_20241611_120159.png'
-=======
-        image_path = 'P3\Results\Data\cum\Andrefunny_20241811_112431.png'
->>>>>>> Stashed changes
-=========
-        image_path = 'P3\Results\Data\Half_Milk\Beside_Camera_20241611_113210.png'
->>>>>>>>> Temporary merge branch 2
+        image_path = 'P3/Results/Data/32th_Milk/Behind_Camera_20241611_121741.png'
         main(cam, image_path, True)
 
     if test_method == 'live':
