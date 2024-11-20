9a01bff057b7b77328e84a5eaa48b886cf62e317
DjangoLan <thor2617@gmail.com>
2024-11-20 15:39:47 +0100

    Pictures

--- P3/Datacollect.py
+++ P3/Datacollect.py
@@ -3,8 +3,29 @@ import os
 import cv2 as cv
 from datetime import datetime
 
-exposure = 278830
+exposure = 66355
 image = im.get_image(exposure)
+
+import matplotlib.pyplot as plt
+plt.imshow(image)
+plt.show()
+
+#make folder in P3/Results/Data
+test_folder = 'Gypsum55g'
+test_name = 'Right_Side'
+light_intensity = '5'
+
+#make folder in P3/Results/Data
+try:
+    os.makedirs(f'P3/Results/Data/{test_folder}')
+except FileExistsError:
+    pass
+
+timestamp = datetime.now().strftime("%Y%d%m_%H%M%S")
+cv.imwrite(f'P3/Results/Data/{test_folder}/{test_name}_light{light_intensity}_exp{im.get_exposure()}_{timestamp}.png', cv.cvtColor(image, cv.COLOR_BGR2RGB))
+
+
+
 #Remember.. exposure = 20-30k, gain = 0 i API
 #image = cv.imread('P3\Results\OrgImages\image_20241311_144042.png')
 
@@ -20,24 +41,3 @@ image = im.get_image(exposure)
 # Dark blue = Beside_Camera
 # Orange = Right_Side
 # Light Blue = InFront_Camera
-
-
-
-
-import matplotlib.pyplot as plt
-plt.imshow(image)
-plt.show()
-
-#make folder in P3/Results/Data
-test_folder = 'Gypsum15g'
-test_name = 'Underwater_Beside_Camera'
-light_intensity = 'x'
-
-#make folder in P3/Results/Data
-try:
-    os.makedirs(f'P3/Results/Data/{test_folder}')
-except FileExistsError:
-    pass
-
-timestamp = datetime.now().strftime("%Y%d%m_%H%M%S")
-cv.imwrite(f'P3/Results/Data/{test_folder}/{test_name}_light{light_intensity}_exp{im.get_exposure()}_{timestamp}.png', cv.cvtColor(image, cv.COLOR_BGR2RGB))
